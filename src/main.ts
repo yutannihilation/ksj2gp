e1d219dbcd35e77879dd567dd665a62783c2f21a
Hiroaki Yutani <yutani.ini@gmail.com>
2025-08-24 08:34:47 +0900

    Fix type

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ const worker = new Worker(new URL("./worker.ts", import.meta.url), {
   type: "module", // It seems this is necessary
 });
 
-document.getElementById("file_picker")!.addEventListener(
+const input = document.getElementById("file_picker") as HTMLInputElement;
+input.addEventListener(
   "change",
   function () {
-    let file = this.files[0];
-    worker.postMessage({ file });
+    if (this.files) {
+      let file = this.files[0];
+      worker.postMessage({ file });
+    }
   },
   false
 );
