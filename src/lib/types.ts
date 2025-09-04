86fbcb4b46988a1d5981703de3a4522e385ffdd7
Hiroaki Yutani <yutani.ini@gmail.com>
2025-09-04 21:02:23 +0900

    Format [skip ci]

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,10 +6,10 @@ export type WorkerRequest = {
 };
 
 export type WorkerResponse = {
-    ready?: boolean;
-    error?: string;
-    handle?: FileSystemFileHandle;
-    // When multiple .shp files are detected in the archive, the worker returns
-    // the list so the main thread can prompt the user to choose one.
-    shp_files?: string[];
+	ready?: boolean;
+	error?: string;
+	handle?: FileSystemFileHandle;
+	// When multiple .shp files are detected in the archive, the worker returns
+	// the list so the main thread can prompt the user to choose one.
+	shp_files?: string[];
 };
