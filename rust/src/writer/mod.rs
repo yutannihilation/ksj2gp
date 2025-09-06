39de5903e14db75a3e6f58d2034d71fd20f4525f
Hiroaki Yutani <yutani.ini@gmail.com>
2025-09-06 10:29:40 +0900

    Fix

--- rust/src/writer/mod.rs
+++ rust/src/writer/mod.rs
@@ -4,7 +4,7 @@ mod geoparquet_writer;
 pub(crate) use geojson_writer::write_geojson;
 pub(crate) use geoparquet_writer::write_geoparquet;
 
+// dBASE fields doesn't include the geometry column
 fn get_fields_except_geometry(x: &[dbase::FieldInfo]) -> Vec<&str> {
-    let (_last, fields_except_geometry) = x.split_last().unwrap();
-    fields_except_geometry.iter().map(|f| f.name()).collect()
+    x.iter().map(|f| f.name()).collect()
 }
