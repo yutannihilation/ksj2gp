dca7fdf4c12d53389fc822a9b03057c74b01d3a1
Hiroaki Yutani <yutani.ini@gmail.com>
2025-10-24 09:30:33 +0900

    Use geoarrow-geojson instead of geojson (#130)
    * Use geoarrow-geojson instead of geojson
    
    * Remove geojson
    
    * Remove serde_json

--- rust/src/transform_coord.rs
+++ rust/src/transform_coord.rs
@@ -1,3 +1,4 @@
+use geo_types::coord;
 use shapefile::Shape;
 
 use crate::{
@@ -13,63 +14,82 @@ impl CoordTransformer {
     pub fn new(src: JapanCrs) -> Self {
         Self { src }
     }
-    pub fn transform_to_geojson(&self, shape: &Shape) -> Result<geojson::Value, Ksj2GpError> {
+    pub fn transform(&self, shape: &Shape) -> Result<geo_types::Geometry, Ksj2GpError> {
         match shape {
-            Shape::Point(point) => Ok(geojson::Value::Point(self.transform_single_point(point)?)),
-            Shape::PointZ(point) => {
-                Ok(geojson::Value::Point(self.transform_single_point_z(point)?))
-            }
+            Shape::Point(point) => Ok(geo_types::Geometry::Point(
+                self.transform_single_point(point)?.into(),
+            )),
+            // Shape::PointZ(point) => {
+            //     Ok(geojson::Value::Point(self.transform_single_point_z(point)?))
+            // }
             Shape::Polyline(polyline) => {
-                let positions = polyline
+                let linestrings = polyline
                     .parts()
                     .iter()
-                    .map(|points| self.transform_points(points))
-                    .collect::<Result<Vec<Vec<geojson::Position>>, _>>()?;
-                Ok(geojson::Value::MultiLineString(positions))
+                    .map(|points| {
+                        self.transform_points(points)
+                            .map(|coords| geo_types::LineString::new(coords))
+                    })
+                    .collect::<Result<Vec<geo_types::LineString>, _>>()?;
+                Ok(geo_types::Geometry::MultiLineString(
+                    geo_types::MultiLineString::new(linestrings),
+                ))
             }
-            Shape::PolylineZ(polyline) => {
-                let positions = polyline
-                    .parts()
-                    .iter()
-                    .map(|points| self.transform_points_z(points))
-                    .collect::<Result<Vec<Vec<geojson::Position>>, _>>()?;
-                Ok(geojson::Value::MultiLineString(positions))
-            }
+            // Shape::PolylineZ(polyline) => {
+            //     let positions = polyline
+            //         .parts()
+            //         .iter()
+            //         .map(|points| self.transform_points_z(points))
+            //         .collect::<Result<Vec<Vec<geojson::Position>>, _>>()?;
+            //     Ok(geojson::Value::MultiLineString(positions))
+            // }
             // TODO: this naively assumes the inner ring always comes first.
             // Need to check the actual specification...
             Shape::Polygon(polygon) => {
-                let positions = polygon
-                    .rings()
-                    .iter()
+                let mut rings = polygon.rings().iter();
+                let exterior = match rings.next() {
+                    Some(ring) => {
+                        let points = ring.points();
+                        let coords = self.transform_points(points)?;
+                        geo_types::LineString::new(coords)
+                    }
+                    None => return Err("Empty polygon".into()),
+                };
+                let interior = rings
                     .map(|ring| {
                         let points = ring.points();
-                        self.transform_points(points)
+                        match self.transform_points(points) {
+                            Ok(coords) => Ok(geo_types::LineString::new(coords)),
+                            Err(e) => Err(e),
+                        }
                     })
-                    .collect::<Result<Vec<Vec<geojson::Position>>, _>>()?;
+                    .collect::<Result<Vec<geo_types::LineString>, _>>()?;
 
-                Ok(geojson::Value::Polygon(positions))
+                Ok(geo_types::Geometry::Polygon(geo_types::Polygon::new(
+                    exterior, interior,
+                )))
             }
 
-            Shape::PolygonZ(polygon) => {
-                let positions = polygon
-                    .rings()
-                    .iter()
-                    .map(|ring| {
-                        let points = ring.points();
-                        self.transform_points_z(points)
-                    })
-                    .collect::<Result<Vec<Vec<geojson::Position>>, _>>()?;
+            // Shape::PolygonZ(polygon) => {
+            //     let positions = polygon
+            //         .rings()
+            //         .iter()
+            //         .map(|ring| {
+            //             let points = ring.points();
+            //             self.transform_points_z(points)
+            //         })
+            //         .collect::<Result<Vec<Vec<geojson::Position>>, _>>()?;
 
-                Ok(geojson::Value::Polygon(positions))
-            }
+            //     Ok(geojson::Value::Polygon(positions))
+            // }
             Shape::Multipoint(multipoint) => {
                 let positions = self.transform_points(multipoint.points())?;
-                Ok(geojson::Value::MultiPoint(positions))
+                Ok(geo_types::Geometry::MultiPoint(positions.into()))
             }
-            Shape::MultipointZ(multipoint) => {
-                let positions = self.transform_points_z(multipoint.points())?;
-                Ok(geojson::Value::MultiPoint(positions))
-            }
+            // Shape::MultipointZ(multipoint) => {
+            //     let positions = self.transform_points_z(multipoint.points())?;
+            //     Ok(geojson::Value::MultiPoint(positions))
+            // }
             _ => Err(format!("Unsupported shape type: {}", shape.shapetype()).into()),
         }
     }
@@ -77,7 +97,7 @@ impl CoordTransformer {
     fn transform_single_point(
         &self,
         point: &shapefile::Point,
-    ) -> Result<geojson::Position, Ksj2GpError> {
+    ) -> Result<geo_types::Coord, Ksj2GpError> {
         // JGD2000, JGD2011 から WGS84 は無変換とする
         match self.src {
             JapanCrs::Tokyo => {
@@ -85,51 +105,51 @@ impl CoordTransformer {
                 // So, we must convert it to radians and then convert back to degree...
                 let mut pt = (point.x.to_radians(), point.y.to_radians());
                 proj4rs::transform::transform(&PROJ4STRING_TOKYO, &PROJ4STRING_WGS84, &mut pt)?;
-                Ok(vec![pt.0.to_degrees(), pt.1.to_degrees()])
+                Ok(coord! { x: pt.0.to_degrees(), y: pt.1.to_degrees() })
             }
-            JapanCrs::JGD2000 | JapanCrs::JGD2011 => Ok(vec![point.x, point.y]),
+            JapanCrs::JGD2000 | JapanCrs::JGD2011 => Ok(coord! { x: point.x, y: point.y }),
         }
     }
 
-    fn transform_single_point_z(
-        &self,
-        point: &shapefile::PointZ,
-    ) -> Result<geojson::Position, Ksj2GpError> {
-        match self.src {
-            JapanCrs::Tokyo => {
-                let mut pt = (
-                    point.x.to_radians(),
-                    point.y.to_radians(),
-                    point.z.to_radians(),
-                );
-                proj4rs::transform::transform(&PROJ4STRING_TOKYO, &PROJ4STRING_WGS84, &mut pt)?;
-                Ok(vec![
-                    pt.0.to_degrees(),
-                    pt.1.to_degrees(),
-                    pt.2.to_degrees(),
-                ])
-            }
-            JapanCrs::JGD2000 | JapanCrs::JGD2011 => Ok(vec![point.x, point.y, point.z]),
-        }
-    }
+    // fn transform_single_point_z(
+    //     &self,
+    //     point: &shapefile::PointZ,
+    // ) -> Result<geo_types::CoordZ, Ksj2GpError> {
+    //     match self.src {
+    //         JapanCrs::Tokyo => {
+    //             let mut pt = (
+    //                 point.x.to_radians(),
+    //                 point.y.to_radians(),
+    //                 point.z.to_radians(),
+    //             );
+    //             proj4rs::transform::transform(&PROJ4STRING_TOKYO, &PROJ4STRING_WGS84, &mut pt)?;
+    //             Ok(vec![
+    //                 pt.0.to_degrees(),
+    //                 pt.1.to_degrees(),
+    //                 pt.2.to_degrees(),
+    //             ])
+    //         }
+    //         JapanCrs::JGD2000 | JapanCrs::JGD2011 => Ok(vec![point.x, point.y, point.z]),
+    //     }
+    // }
 
     fn transform_points(
         &self,
         points: &[shapefile::Point],
-    ) -> Result<Vec<geojson::Position>, Ksj2GpError> {
+    ) -> Result<Vec<geo_types::Coord>, Ksj2GpError> {
         points
             .iter()
             .map(|point| self.transform_single_point(point))
-            .collect::<Result<Vec<geojson::Position>, _>>()
+            .collect::<Result<Vec<geo_types::Coord>, _>>()
     }
 
-    fn transform_points_z(
-        &self,
-        points: &[shapefile::PointZ],
-    ) -> Result<Vec<geojson::Position>, Ksj2GpError> {
-        points
-            .iter()
-            .map(|point| self.transform_single_point_z(point))
-            .collect::<Result<Vec<geojson::Position>, _>>()
-    }
+    // fn transform_points_z(
+    //     &self,
+    //     points: &[shapefile::PointZ],
+    // ) -> Result<Vec<geojson::Position>, Ksj2GpError> {
+    //     points
+    //         .iter()
+    //         .map(|point| self.transform_single_point_z(point))
+    //         .collect::<Result<Vec<geojson::Position>, _>>()
+    // }
 }
--- rust/src/writer/geojson_writer.rs
+++ rust/src/writer/geojson_writer.rs
@@ -1,14 +1,15 @@
 use std::io::{Read, Seek, Write};
 
-use geojson::JsonObject;
+use itertools::Itertools as _;
 
 use crate::{
-    crs::JapanCrs,
-    error::Ksj2GpError,
-    transform_coord::CoordTransformer,
-    translate::{TranslateOptions, get_codelist_map, translate_colnames},
+    builder::construct_schema, crs::JapanCrs, error::Ksj2GpError,
+    transform_coord::CoordTransformer, translate::TranslateOptions,
     writer::get_fields_except_geometry,
 };
+
+// Number of rows to process at once
+const CHUNK_SIZE: usize = 2048;
 
 pub(crate) fn write_geojson<T: Read + Seek, D: Read + Seek, W: Write + Send>(
     reader: &mut shapefile::Reader<T, D>,
@@ -17,123 +18,44 @@ pub(crate) fn write_geojson<T: Read + Seek, D: Read + Seek, W: Write + Send>(
     crs: JapanCrs,
     translate_options: &TranslateOptions,
 ) -> Result<(), Ksj2GpError> {
-    let transformer = CoordTransformer::new(crs);
+    // TODO: include this in FieldsWithGeo
+    let transformer = CoordTransformer::new(crs.clone());
+
+    let projjson = crs.to_projjson();
+    let crs = geoarrow_schema::Crs::from_projjson(projjson.into());
+
+    let fields_info = construct_schema(dbf_fields, crs, translate_options)?;
+    let schema_ref = fields_info.schema_ref.clone();
 
     // Since shapefile::Record is a HashMap, the iterator of it doesn't maintain
     // the order. So, this column names vector is needed to ensure the consistent
     // order with the schema.
     let field_names = get_fields_except_geometry(dbf_fields);
 
-    let mut features: Vec<geojson::Feature> = Vec::new();
-    for result in reader.iter_shapes_and_records() {
-        let (shape, mut record) = result.unwrap();
+    let mut geojson_writer = geoarrow_geojson::writer::GeoJsonWriter::new(writer)?;
 
-        // Convert dBASE record to GeoJSON properties without serde dependency
-        let mut properties: JsonObject = JsonObject::new();
-        for field_name in &field_names {
-            let value = record
-                .remove(field_name)
-                .ok_or_else(|| format!("Not found {field_name}"))?;
+    for chunk in &reader.iter_shapes_and_records().chunks(CHUNK_SIZE) {
+        let mut builders = fields_info.create_builders(CHUNK_SIZE);
 
-            let translated_field_name = translate_colnames(field_name, translate_options)?;
+        for result in chunk {
+            let (shape, mut record) = result.unwrap();
 
-            properties.insert(
-                translated_field_name,
-                dbase_field_to_json_value(
-                    value,
-                    field_name,
-                    translate_options.translate_contents,
-                    translate_options.year,
-                    &translate_options.target_shp,
-                ),
-            );
+            for (i, field_name) in field_names.iter().enumerate() {
+                let value = record
+                    .remove(field_name)
+                    .ok_or_else(|| format!("Not found {field_name}"))?;
+                builders.builders[i].push(value);
+            }
+
+            let geometry = transformer.transform(&shape)?;
+            builders.geo_builder.push_geometry(Some(&geometry))?;
         }
 
-        let geometry = geojson::Geometry::new(transformer.transform_to_geojson(&shape)?);
-
-        features.push(geojson::Feature {
-            bbox: None,
-            geometry: Some(geometry),
-            id: None,
-            properties: Some(properties),
-            foreign_members: None,
-        });
+        let batch = arrow_array::RecordBatch::try_new(schema_ref.clone(), builders.finish())?;
+        geojson_writer.write(&batch)?;
     }
 
-    let geojson: geojson::GeoJson = geojson::FeatureCollection {
-        bbox: None,
-        features,
-        foreign_members: None,
-    }
-    .into();
-
-    // TODO: implement From<geojson::Error>
-    let geojson_str = geojson.to_string_pretty().unwrap();
-
-    writer.write_all(geojson_str.as_bytes())?;
-    writer.flush()?;
+    geojson_writer.finish()?;
 
     Ok(())
-}
-
-fn dbase_field_to_json_value(
-    x: dbase::FieldValue,
-    field_name: &str,
-    translate_contents: bool,
-    year: u16,
-    target_shp: &str,
-) -> geojson::JsonValue {
-    if translate_contents && let Some(codelist_map) = get_codelist_map(field_name, year, target_shp)
-    {
-        let code: String = match x {
-            // String
-            dbase::FieldValue::Character(Some(x)) => x.into(),
-            dbase::FieldValue::Memo(x) => x.into(),
-            // Number
-            dbase::FieldValue::Numeric(Some(x)) | dbase::FieldValue::Double(x) => format!("{x:.0}"),
-            dbase::FieldValue::Float(Some(x)) => format!("{x:.0}"),
-            dbase::FieldValue::Integer(x) => format!("{x:.0}"),
-            // TODO: raise error for unexpected type
-            _ => return geojson::JsonValue::Null,
-        };
-
-        match codelist_map.get(code.as_str()) {
-            Some(label) => label.to_string().into(),
-            // TODO: raise error for unexpected value
-            None => geojson::JsonValue::Null,
-        }
-    } else {
-        match x {
-            // String
-            dbase::FieldValue::Character(x) => x.into(),
-            dbase::FieldValue::Memo(x) => x.into(),
-            // Number
-            dbase::FieldValue::Numeric(x) => x.into(),
-            dbase::FieldValue::Float(x) => x.into(),
-            dbase::FieldValue::Integer(x) => x.into(),
-            dbase::FieldValue::Double(x) => x.into(),
-            dbase::FieldValue::Currency(x) => x.into(),
-            // Boolean
-            dbase::FieldValue::Logical(x) => x.into(),
-            // Date
-            dbase::FieldValue::Date(Some(x)) => {
-                format!("{}-{}-{}", x.year(), x.month(), x.day()).into()
-            }
-            dbase::FieldValue::Date(None) => geojson::JsonValue::Null,
-            dbase::FieldValue::DateTime(x) => {
-                let date = x.date();
-                let time = x.time();
-                format!(
-                    "{}-{}-{} {}:{}:{}",
-                    date.year(),
-                    date.month(),
-                    date.day(),
-                    time.hours(),
-                    time.minutes(),
-                    time.seconds()
-                )
-                .into()
-            }
-        }
-    }
 }
