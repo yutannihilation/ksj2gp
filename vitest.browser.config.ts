532132d55392db4675d00b82743bcba221615483
Hiroaki Yutani <yutani.ini@gmail.com>
2025-12-30 10:13:56 +0900

    Use vitest

---
+++ vitest.browser.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+	plugins: [svelte()],
+	test: {
+		browser: {
+			enabled: true,
+			provider: playwright(),
+			// https://vitest.dev/config/browser/playwright
+			instances: [{ browser: 'chromium' }]
+		}
+	}
+});
